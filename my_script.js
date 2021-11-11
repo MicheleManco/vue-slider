da9a25ada5afd340cf9c25f601897bf9e46c8578
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-11-11 14:43:59 +0100

    finito

---
+++ my_script.js
@@ -0,0 +1,61 @@
+var miovue = new Vue({
+    el: "#container",
+    data: {
+        countSlides: 0,
+        slides: [
+            {
+              image: 'img/01.jpg',
+              title: 'Svezia',
+              text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'londra',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'giappone',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'tunisia',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'america',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            }
+        ],
+    },
+    methods: {
+            nextSlide: function(){
+                if(this.countSlides === this.slides.length -1){
+                    this.countSlides = 0;
+                }   else {
+                    this.countSlides++
+                }
+            },
+            prevSlide:function(){
+                if(this.countSlides === 0){
+                    this.countSlides = this.slides.length -1;
+                }   else {
+                    this.countSlides--
+                }
+            }   
+
+        }   
+    
+});
+
+
+
+
+
+
+
+
+
+
